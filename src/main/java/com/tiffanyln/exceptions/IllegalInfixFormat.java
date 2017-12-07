c9c5f6bdad5f0a87467b5f232167f0b7579d2af1
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-12-06 23:57:10 -0500

    :sparkles: Add exception

---
+++ src/main/java/com/tiffanyln/exceptions/IllegalInfixFormat.java
@@ -0,0 +1,25 @@
+package com.tiffanyln.exceptions;
+
+public class IllegalInfixFormat extends Exception {
+
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public IllegalInfixFormat() {
+        super("The infix queue is not properly formatted");
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public IllegalInfixFormat(String message) {
+        super(message);
+    }
+}
